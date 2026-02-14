dc000ec2cd562ae00d4ecdc413a31a421c36785b
REALERvolker1 <73304952+REALERvolker1@users.noreply.github.com>
2026-02-14 12:54:30 -0600

    more direction functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
 #![no_std]
 
 macro_rules! enum_matcher_array {
-    ($vis:vis const $arrayname:ident: $arrayty:ty = { @default $default:expr; $( $variant:ident => $rhs:expr, )+ }) => {
+    (
+        $( #[$meta:meta] )*
+        $vis:vis const $arrayname:ident: $arrayty:ty =
+        {
+            @default $default:expr;
+            $( $variant:ident => $rhs:expr, )+
+        }
+    ) => {
+        $( #[$meta] )*
         $vis const $arrayname: [$arrayty; Self::COUNT] = {
             let mut out = [$default; _];
             let mut i = 0;
@@ -19,6 +27,81 @@ macro_rules! enum_matcher_array {
 }
 
 macro_rules! enum_ordered_array {
+    (@inner_to_primitive $enum:ident) => {
+        enum_ordered_array!(@inner_from $enum, u8);
+        enum_ordered_array!(@inner_from $enum, i8);
+        enum_ordered_array!(@inner_from $enum, u16);
+        enum_ordered_array!(@inner_from $enum, i16);
+        enum_ordered_array!(@inner_from $enum, u32);
+        enum_ordered_array!(@inner_from $enum, i32);
+        enum_ordered_array!(@inner_from $enum, u64);
+        enum_ordered_array!(@inner_from $enum, i64);
+        enum_ordered_array!(@inner_from $enum, u128);
+        enum_ordered_array!(@inner_from $enum, i128);
+        enum_ordered_array!(@inner_from $enum, usize);
+        enum_ordered_array!(@inner_from $enum, isize);
+
+        #[cfg(feature = "num-traits")]
+        impl ::num_traits::ToPrimitive for $enum {
+            fn to_u8(&self) -> Option<u8> {
+                Some((*self).into())
+            }
+            fn to_i8(&self) -> Option<i8> {
+                Some((*self).into())
+            }
+            fn to_u16(&self) -> Option<u16> {
+                Some((*self).into())
+            }
+            fn to_i16(&self) -> Option<i16> {
+                Some((*self).into())
+            }
+            fn to_u32(&self) -> Option<u32> {
+                Some((*self).into())
+            }
+            fn to_i32(&self) -> Option<i32> {
+                Some((*self).into())
+            }
+            fn to_u64(&self) -> Option<u64> {
+                Some((*self).into())
+            }
+            fn to_i64(&self) -> Option<i64> {
+                Some((*self).into())
+            }
+            fn to_u128(&self) -> Option<u128> {
+                Some((*self).into())
+            }
+            fn to_i128(&self) -> Option<i128> {
+                Some((*self).into())
+            }
+            fn to_usize(&self) -> Option<usize> {
+                Some((*self).into())
+            }
+            fn to_isize(&self) -> Option<isize> {
+                Some((*self).into())
+            }
+            fn to_f32(&self) -> Option<f32> {
+                let n: i32 = (*self).into();
+                n.to_f32()
+            }
+            fn to_f64(&self) -> Option<f64> {
+                let n: i64 = (*self).into();
+                n.to_f64()
+            }
+        }
+    };
+    (@inner_from $enum:ident, $ty:ty) => {
+        impl From<$enum> for $ty {
+            fn from(value: $enum) -> Self {
+                value as _
+            }
+        }
+        #[cfg(feature = "num-traits")]
+        impl ::num_traits::AsPrimitive<$ty> for $enum {
+            fn as_(self) -> $ty {
+                self as _
+            }
+        }
+    };
     (
         $( #[$meta:meta] )*
         $vis:vis enum $enum:ident {
@@ -87,11 +170,18 @@ macro_rules! enum_ordered_array {
                 f.write_str(self.to_str())
             }
         }
+        enum_ordered_array!(@inner_to_primitive $enum);
     };
 }
 
 enum_ordered_array! {
-    #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+    /// A simple direction selection enum, designed for matching, selecting, indexing, etc.
+    /// For more complex 2D or 3D scenes, consider using a vector instead.
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+    #[cfg_attr(feature = "serde", derive(serde_derive::Serialize, serde_derive::Deserialize))]
+    #[cfg_attr(feature = "defmt", derive(defmt::Format))]
+    #[cfg_attr(feature = "bevy_reflect", derive(bevy_reflect::Reflect))]
+    #[cfg_attr(feature = "bevy_ecs", derive(bevy_ecs::component::Component))]
     pub enum Direction {
         Up = 0,
         Right = 1,
@@ -105,6 +195,7 @@ enum_ordered_array! {
 
 impl Direction {
     enum_matcher_array! {
+        /// An array of variants, each corresponding to its own "opposite", or "reverse".
         pub const VARIANTS_OPPOSITE: Self = {
             @default Self::Up;
             Back => Self::Front,
@@ -115,19 +206,20 @@ impl Direction {
             Down => Self::Up,
         }
     }
-}
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    /// Get the opposite of the provided `Direction`
+    /// ```
+    /// use directionlib::Direction;
+    ///
+    /// let current = Direction::Front;
+    ///
+    /// assert_eq!(current.reverse(), Direction::Back);
+    ///
+    /// let vertical = Direction::Down;
+    ///
+    /// assert_eq!(vertical.reverse(), Direction::Up);
+    /// ```
+    pub const fn reverse(self) -> Self {
+        Self::VARIANTS_OPPOSITE[self as usize]
     }
 }
